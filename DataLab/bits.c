d8b1010ad81943c9536d33a621cfd7d3b94f1823
Leon <Liangz.org@gmail.com>
2020-09-06 23:26:44 +0800

    feat: return minimum two's complement integer

--- DataLab/bits.c
+++ DataLab/bits.c
@@ -12,13 +12,31 @@
 
 #include <stdio.h>
 
-
+/*
+ * bitXor - x^y using only ~ and &
+ *   Example: bitXor(4, 5) = 1
+ *   Legal ops: ~ &
+ *   Max ops: 14
+ *   Rating: 1
+ */
 int bitXor(int x, int y) {
     return ~(~x & ~y) & ~(x & y);
 }
 
+/*
+ * tmin - return minimum two's complement integer
+ *   Legal ops: ! ~ & ^ | + << >>
+ *   Max ops: 4
+ *   Rating: 1
+ */
+int tmin(void) {
+    return 0x1<<31;
+}
+
+
 int main() {
     printf("%d\n", bitXor(3, 2));
     printf("%d\n", 3 ^ 2);
+    printf("%d\n", tmin());
     return 0;
 }
